1127aa96f94fda5aa781fd988844d0f03270c0d1
Evan Pacini <evan.pacini@gmail.com>
2022-10-30 11:36:02 +0100

    Simplify arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,28 +1,17 @@
-const int IN1 = 2;
-const int IN2 = 3;
-const int IN3 = 4;
-const int IN4 = 5;
+const int RELAY = 2;
 
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
-
-  Serial.begin(9600);
+  pinMode(RELAY, OUTPUT);       // Enable for OUTPUT
+  digitalWrite(RELAY, HIGH);    // Relay is open on HIGH
+  Serial.begin(9600);           // Wait for serial messages
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    Serial.read();
-    Serial.println("1");
-    digitalWrite(IN1, LOW);
-    delay(50);
-    digitalWrite(IN1, HIGH);
-    delay(50);
+  if (Serial.available() > 0) { // If a byte was sent
+    Serial.read();              // Get rid of byte
+    digitalWrite(RELAY, LOW);   // Close the relay
+    delay(50);                  // Wait for 50 ms before opening
+    digitalWrite(RELAY, HIGH);  // Open the relay
+    delay(50);                  // Wait for 50 ms before processing any other bytes
   }
 }
