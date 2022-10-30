e6e1d0f0823c71461eaecf3ad0b08077cc754c6c
Evan Pacini <evan.pacini@gmail.com>
2022-10-30 11:04:55 +0100

    The mod now has error handling and automatically tries to reconnect and can be used outside the test environment!

--- src/main/java/works/moes/shocking/shocking/Shocking.java
+++ src/main/java/works/moes/shocking/shocking/Shocking.java
@@ -2,19 +2,36 @@ package works.moes.shocking.shocking;
 
 import com.fazecast.jSerialComm.SerialPort;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Shocking implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("Shocking");
     public static SerialPort serialPort;
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
+        LOGGER.info("Loading Shocking...");
+        open();
+    }
 
-        System.out.println("[Shocking] Hello Fabric world!");
-        serialPort = SerialPort.getCommPort("/dev/ttyACM0");
-        serialPort.setComPortParameters(9600, 8, 1, 0);
-        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-        serialPort.openPort();
+    public static void send(byte[] data) {
+        if (serialPort == null || !serialPort.isOpen()) open();
+        try {
+            serialPort.writeBytes(data, data.length);
+        } catch (Exception e) {
+            LOGGER.error("Error writing to serial port: " + e.getMessage());
+        }
+    }
+
+    public static void open() {
+        try {
+            LOGGER.info("Initialising serial port...");
+            serialPort = SerialPort.getCommPort("/dev/ttyACM0");
+            serialPort.setComPortParameters(9600, 8, 1, 0);
+            serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            serialPort.openPort();
+        } catch (Exception e) {
+            LOGGER.error("Error opening serial port: " + e.getMessage());
+        }
     }
 }
--- src/main/java/works/moes/shocking/shocking/mixin/ShockingMixin.java
+++ src/main/java/works/moes/shocking/shocking/mixin/ShockingMixin.java
@@ -1,17 +1,19 @@
 package works.moes.shocking.shocking.mixin;
 
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import works.moes.shocking.shocking.Shocking;
+import static java.lang.Math.min;
 
 @Mixin(PlayerEntity.class)
-public class ShockingMixin {
-    @Inject(at = @At("TAIL"), method = "applyDamage")
-    private void applyDamage(CallbackInfo info) {
-        System.out.println("Ouch! I'm hurt!");
-        Shocking.serialPort.writeBytes("1".getBytes(), 1);
+public abstract class ShockingMixin {
+    @Inject(method = "applyDamage", at = @At("TAIL"))
+    private void applyDamage(DamageSource source, float amount, CallbackInfo ci) {
+        Shocking.LOGGER.info("Ouch! I'm hurt! Amount: " + min(amount, 20f));
+        Shocking.send(new byte[(int) min(amount, 20f)]);
     }
 }
